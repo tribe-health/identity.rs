ab2fc56ea86da2a11ed369a330dcc75e662293e5
l1h3r <devin.turner@iota.org>
2021-01-14 08:44:13 -0800

    Add generic merkle tree

---
+++ identity_core/src/crypto/merkle_tree/consts.rs
@@ -0,0 +1,7 @@
+use core::mem;
+
+// https://en.wikipedia.org/wiki/Merkle_tree#Second_preimage_attack
+pub const PREFIX_L: &[u8] = &[0x00];
+pub const PREFIX_B: &[u8] = &[0x01];
+
+pub const SIZE_USIZE: usize = mem::size_of::<usize>();
